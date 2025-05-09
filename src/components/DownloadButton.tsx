dc66269d81a293dd81d766dad2b5e2ac18d238f5
Piotr Obara <pobara@o2.pl>
2025-05-09 17:37:37 +0200

    configure Firebase Hosting for deployment

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { IoMdDownload } from 'react-icons/io';
 import { DownloadButtonProps } from '../interfaces';
 
-
-
 const DownloadButton: React.FC<DownloadButtonProps> = ({ filePath, label }) => {
+  const downloadLink = process.env.PUBLIC_URL + filePath;
+
   return (
     <a
-      href={filePath}
+      href={downloadLink}
       download
       className="inline-flex items-center bg-black text-white hover:bg-gray-800 px-3 py-1.5 transition-colors gap-1.5 font-roboto-condensed mt-2 w-auto"
       aria-label="Pobierz CV"
